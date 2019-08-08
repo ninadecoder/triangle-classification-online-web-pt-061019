3646b84a69cc7251e7e68b587966e073c1efc0b4
Nina Kazzim <kazzim@icloud.com>
2019-08-08 16:57:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,89 +1,25 @@
 class Triangle
-
-  attr_accessor :sides
-
-  def initialize(side1, side2, side3)
-    @sides = [side1, side2, side3]
-    @kind = set_kind
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
   end
-
-  def check
-    valid?
-  end
-
+  
   def kind
-    raise TriangleError.new if !valid?
-
-    @kind
-  end
-
-  # private
-
-  def valid?
-    return false if @sides.any?{|side| side <= 0}
-    case @kind
-    when Types.scalene
-      return longest < @sides.reject{|side| side == longest}.reduce{|cum, cur| cum + cur}
-    when Types.isosceles
-      larger = @sides.reject{|side| side == smallest}
-      puts larger.length
-      puts smallest * 2
-      return larger.length == 2 || smallest * 2 > larger[0]
-    when Types.equilateral
-      return true
-    else
-      puts "This should never be run"
-    end
-  end
-
-  def set_kind
-    case unique_sides
-    when 1
-      return Types.equilateral
-    when 2
-      return Types.isosceles
-    when 3
-      return Types.scalene
-    else
-      raise TraingleError
-    end
-
-  end
-
-  def longest
-    largest = 0
-    @sides.each{|side| largest = side if side > largest}
-    largest
-  end
-
-  def smallest
-    smallest = Float::INFINITY
-    @sides.each{|side| smallest = side if side < smallest}
-    smallest
-  end
-
-  def unique_sides
-    Set.new(@sides).size
-  end
-
-  class Types
-
-    def self.isosceles
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
+      raise TriangleError
+    elsif (@side_one + @side_two) < @side_three || (@side_one + @side_three) <= @side_two || (@side_two + @side_three) < @side_one
+      raise TriangleError
+    elsif @side_one == @side_two && @side_one == @side_three
+      :equilateral
+    elsif @side_two == @side_three || @side_one == @side_three || @side_one == @side_two
       :isosceles
-    end
-
-    def self.scalene
+    else
       :scalene
     end
-
-    def self.equilateral
-      :equilateral
-    end
-
   end
-
+  
   class TriangleError < StandardError
-
   end
-
 end
